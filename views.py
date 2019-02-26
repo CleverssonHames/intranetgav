a90fe01710de4283563c8585936866891818bae3
leofregnani <39562166+leofregnani@users.noreply.github.com>
2019-02-26 09:43:13 -0400

    Populando Combobox Destino V.2

--- views.py
+++ views.py
@@ -63,5 +63,5 @@ def etiqueta_correio(request):
 	empresas = Filial.objects.all().order_by('filial')
 	estados = Filial.objects.values('estado').distinct()
 	cidades = Filial.objects.values('cidade').distinct()
-	return render(request, 'intranet/etiqueta_correio.html', {'empresas':empresas, 'cidades': cidades, 'estados':estados})
+	return render(request, 'intranet/etiqueta_correios.html', {'empresas':empresas, 'cidades': cidades, 'estados':estados})
 	# return render(request, 'intranet/etiqueta_correios.html', {})
